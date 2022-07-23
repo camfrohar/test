54d68228cafc62a6179e0904aa52362da8e7c77c
camfrohar <camfrohar@yahoo.com>
2022-07-22 21:52:01 -0400

    test

--- 16c750_support.h
+++ 16c750_support.h
@@ -1,1 +1,124 @@
 
+#define UART_RHR_REG	0x000
+#define UART_THR_REG	0x000
+#define UART_IER_REG	0x004
+#define UART_IIR_REG	0x008
+#define UART_FCR_REG	0x008
+#define UART_LCR_REG	0x00C
+#define UART_MCR_REG	0x010
+#define UART_LSR_REG	0x014
+#define UART_MSR_REG	0x018
+#define UART_SPR_REG	0x01C
+#define UART_MDR1_REG	0x020
+#define UART_MDR2_REG	0x024
+#define UART_MDR3_REG	0x080
+#define UART_SYSC_REG	0x054
+#define UART_SYSS_REG	0x058
+#define UART_RXFIFO_LVL_REG	0x064
+#define UART_TXFIFO_LVL_REG	0x068
+#define UART_DLL_REG	0x100
+#define UART_DLH_REG	0x104
+#define UART_EFR_REG	0x208
+
+// This function writes a 16-bit value to the UART register with byte offset reg_offset 
+static void write_uart_reg_raw (u32 reg_offset, u16 value);
+
+// This function reads a 16-bit value from the UART register with the byte offset reg_offset
+static u16 read_uart_reg_raw (u32 reg_offset);
+
+static void uart_deinit(void);
+
+int init_uart_reg (void);
+
+static u16 cached_lcr = 0;
+
+void write_uart_reg (u32 reg, u16 value)
+{
+        u16 lcr_selector_index = (reg / 256) % 3;
+        u16 reg_offset = (reg % 256);
+
+        if (lcr_selector_index == 1)
+                write_uart_reg_raw (UART_LCR_REG, cached_lcr | 0x80);
+        else if (lcr_selector_index == 2)
+                write_uart_reg_raw (UART_LCR_REG, 0xBF);
+
+        write_uart_reg_raw (reg_offset, value);
+        if (reg_offset == UART_LCR_REG)
+                cached_lcr = value;
+
+        if (lcr_selector_index != 0)
+                write_uart_reg_raw (UART_LCR_REG, cached_lcr);
+}
+
+u16 read_uart_reg (u32 reg)
+{
+        u16 value = 0;
+        u16 lcr_selector_index = (reg / 256) % 3;
+        u16 reg_offset = (reg % 256);
+
+        if (lcr_selector_index == 1)
+                write_uart_reg_raw (UART_LCR_REG, cached_lcr | 0x80);
+        else if (lcr_selector_index == 2)
+                write_uart_reg_raw (UART_LCR_REG, 0xBF);
+
+        value = read_uart_reg_raw (reg_offset);
+
+        if (lcr_selector_index != 0)
+                write_uart_reg_raw (UART_LCR_REG, cached_lcr);
+
+        return value;
+}
+
+int init_uart_reg (void)
+{
+	uint32_t val;
+        write_uart_reg_raw(UART_LCR_REG, cached_lcr);
+
+	write_uart_reg(UART_SYSC_REG,0x0002);
+
+	do {
+		val = read_uart_reg(UART_SYSC_REG);
+	} while (!(val & 0x01)); // Wait for bit 0
+
+
+	write_uart_reg(UART_MCR_REG,0x0010);
+	write_uart_reg(UART_FCR_REG,0x0007);
+	write_uart_reg(UART_LCR_REG,0x0003);
+	write_uart_reg(UART_DLH_REG,0x0001);
+	write_uart_reg(UART_MCR_REG,0x0017);
+	write_uart_reg(UART_MDR1_REG,0x0000);
+
+	val = read_uart_reg(UART_MCR_REG);
+	printk(KERN_INFO "UART MCR Register Value = 0x%04x\n", val);
+	
+	if(val != 0x0017) {
+		printk(KERN_ERR "UART MCR Register Verification Failed \n");
+		return -EIO;
+	}
+
+	write_uart_reg(UART_THR_REG,0x0041);
+
+	do {
+		val = read_uart_reg(UART_RXFIFO_LVL_REG);
+	} while ((val & 0x1F) == 0); // Wait for non-zero
+
+	val = read_uart_reg(UART_RHR_REG);
+	printk(KERN_INFO "Received value from RX FIFO = 0x%04x\n", val & 0x00FF);
+
+	if((val & 0x00FF) != 0x41) {
+		printk(KERN_ERR "UART MCR Register Verification Failed \n");
+		uart_deinit();
+		return -EIO;
+	}
+
+
+
+	printk(KERN_INFO "UART MCR Register Verification Complete!!");
+
+
+	return 0;
+
+
+
+}
+
--- uart_loop.c
+++ uart_loop.c
@@ -1,1 +1,182 @@
+#include <linux/module.h>
+#include <linux/kernel.h>
+#include <linux/init.h>
+#include <linux/platform_device.h>
+#include <linux/sysfs.h>
+#include <linux/string.h>
+#include <linux/io.h>
+#include "16c750_support.h"
+ 
+
+
+#define DEFAULT_BPS 115200
+#define UART2_BASE_PHYS_ADDR 0x48022000
+#define UART2_REG_SIZE 0x1000
+
+
+static unsigned long bps_rate = DEFAULT_BPS;
+module_param(bps_rate, long, 0444);
+
+
+struct barrometer_data {
+	char loopback[16];
+	void __iomem *uart2_base; // Virtual Memory Base Address
+}; 
+
+static struct barrometer_data barrometer;
+
+static int barrometer_probe(struct platform_device *pdev);
+static int barrometer_remove(struct platform_device *pdev);
+static int uart_init(void);
+static void uart_deinit(void);
+static void write_uart_reg_raw(u32 reg_offset, u16 value);
+static u16 read_uart_reg_raw(u32 reg_offset);
+
+
+static struct platform_driver barrometer_driver = {
+	.probe = barrometer_probe, 
+	.remove = __devexit_p(barrometer_remove),
+	.driver = {
+		.name = "barrometer_uart2",
+	},
+};
+
+
+static int uart_init(void) {
+	barrometer.uart2_base = ioremap_nocache(UART2_BASE_PHYS_ADDR, UART2_REG_SIZE); //Maps a physical address range into kernel's virtual memory
+	if(!barrometer.uart2_base) {
+		printk(KERN_ERR "Failed to map UART2 Registers \n");
+		return -ENOMEM;
+	}
+	printk(KERN_INFO "uart_loop: UART2 Registers mapped successfully! \n");
+	return 0;
+}
+
+static void uart_deinit(void) {
+	if(barrometer.uart2_base) {
+		iounmap(barrometer.uart2_base);
+		printk(KERN_INFO "uart_loop: UART2 Registers unmapped successfully! \n");
+	}
+}
+
+
+static void write_uart_reg_raw(u32 reg_offset, u16 value) {
+	
+	if(barrometer.uart2_base){
+		iowrite16(value, barrometer.uart2_base + reg_offset);
+		printk(KERN_INFO "Wrote 0x%04x to UART register at offset 0x%08x \n", value, reg_offset);
+	} else{
+		printk(KERN_ERR "UART Register is not mapped!!\n");
+	}
+}
+
+
+static u16 read_uart_reg_raw(u32 reg_offset){
+	u16 value = 0;
+	
+	if(barrometer.uart2_base){
+		ioread16(barrometer.uart2_base + reg_offset);
+		printk(KERN_INFO "Read 0x%04x to UART register at offset 0x%08x \n", value, reg_offset);
+	} else{
+		printk(KERN_ERR "UART Register is not mapped!!\n");
+	}
+	return value;
+}
+
+
+// Initial Function called (__init)
+static int __init init_callback_fn(void){
+
+	printk(KERN_INFO "uart_loop: Module loaded successfully! \n");
+	printk(KERN_INFO "BPS_RATE = %lu \n", bps_rate);
+	return platform_driver_register(&barrometer_driver); //Passing in the barrometer_driver struct
+
+}
+
+// Last Function called (__exit)
+static void __exit exit_callback_fn(void) {
+	printk(KERN_INFO "uart_loop: Module unloaded successfully! \n");
+	platform_driver_unregister(&barrometer_driver);
+}
+
+
+// Read a Device Attribute Value
+static ssize_t loopback_show(struct device *dev, struct device_attribute *attr, char *buf)
+{
+	return sprintf(buf, "%s\n", barrometer.loopback);
+}
+
+
+// Write a Device Attribute Value
+static ssize_t loopback_store(struct device *dev, struct device_attribute *attr, const char *buf, size_t count)
+{
+	if(strncmp(buf,"on",2) == 0) {
+		strncpy(barrometer.loopback, "on", sizeof(barrometer.loopback));
+	} else if(strncmp(buf,"off",3) == 0){
+		strncpy(barrometer.loopback, "off", sizeof(barrometer.loopback));
+	} else {
+		return -EINVAL; //Invalid Argument Error Code
+	}
+	
+	barrometer.loopback[sizeof(barrometer.loopback) -1] = '\0'; //Setting null character	
+
+	return count;
+}
+
+// Linking the attribute to the show and store functions
+static DEVICE_ATTR(loopback, 0644, loopback_show, loopback_store); //0644 allows Read and Write. 0444 only read.
+
+
+// Binds the driver to the specific hardware device
+static int barrometer_probe(struct platform_device *pdev){
+	int retval;
+	init_uart_reg();
+
+
+	printk(KERN_INFO "uart_loop: Barrometer Probe Function called! \n");
+
+	retval = uart_init();
+	if(retval){
+		printk(KERN_ERR "Failed to initialize UART2\n");
+		return retval;
+	}
+
+	
+	retval = device_create_file(&pdev->dev, &dev_attr_loopback); // Allows users to interact with attribute through /sys
+
+	if (retval) {
+		printk(KERN_INFO "uart_loop: Failed to create sysfs file! \n");
+		uart_deinit();
+		return retval;
+	}
+
+	strncpy(barrometer.loopback, "off", sizeof(barrometer.loopback));
+
+	printk(KERN_INFO "uart_loop: Driver bounded successfully! \n");
+
+	return 0;
+}
+
+
+
+static int barrometer_remove(struct platform_device *pdev){
+	printk(KERN_INFO "uart_loop: Barrometer Remove Function called! \n");
+
+	device_remove_file(&pdev->dev, &dev_attr_loopback);
+	uart_deinit();
+
+	printk(KERN_INFO "uart_loop: Driver unbounded successfully! \n");
+	return 0;
+}
+
+
+
+module_init(init_callback_fn);
+module_exit(exit_callback_fn);
+
+
+MODULE_LICENSE("GPL");
+MODULE_AUTHOR("Cameron Frohar");
+MODULE_DESCRIPTION("Kernel module for uart loopback");
+MODULE_VERSION("1.0");
 
